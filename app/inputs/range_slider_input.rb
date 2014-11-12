151d4baf0cdcedc5f0490032ff335dc6c6498a38
Zequez <zequez@gmail.com>
2014-11-12 18:25:51 -0300

    Delete an old file, remove modal from DOM

--- app/inputs/range_slider_input.rb
+++
@@ -1,9 +0,0 @@
-class RangeSliderInput < Formtastic::Inputs::SliderInput
-  def to_html
-    input_wrapping do
-      label_html <<
-      name_html <<
-      builder.file_field(method, input_html_options)
-    end
-  end
-end
