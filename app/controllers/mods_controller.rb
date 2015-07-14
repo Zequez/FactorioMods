fe2bfb54035c7b507571336077572ef4abd23831
Zequez <zequez@gmail.com>
2015-07-14 03:44:30 -0300

    Fix source URL

--- app/controllers/mods_controller.rb
+++ app/controllers/mods_controller.rb
@@ -65,6 +65,7 @@ class ModsController < ApplicationController
     if params[:forum_post_id]
       forum_post = ForumPost.find params[:forum_post_id]
       @mod.name = forum_post.title
+      
       @mod.author_name = forum_post.author_name
       @mod.forum_url = forum_post.url
       if forum_post.published_at
