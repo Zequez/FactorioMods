c18145cb93d0f67215dcc91daf85184bd93f8a0d
Zequez <zequez@gmail.com>
2015-08-06 16:01:22 -0300

    Fix some forum_validations tests

--- spec/views/forum_validations/show_spec.rb
+++ spec/views/forum_validations/show_spec.rb
@@ -1,9 +1,9 @@
 describe 'forum_validations/show', type: :view do
   before :each do
     @fv = create :forum_validation
-    create :mod, authors: [@fv.author], name: 'Potato'
-    create :mod, authors: [@fv.author], name: 'Salad'
-    create :mod, authors: [@fv.author], name: 'Simulator'
+    @m1 = create :mod, authors: [@fv.author], name: 'Potato'
+    @m2 = create :mod, authors: [@fv.author], name: 'Salad'
+    @m3 = create :mod, authors: [@fv.author], name: 'Simulator'
   end
 
   it 'display the list of mods associated with the author' do
@@ -18,6 +18,15 @@ describe 'forum_validations/show', type: :view do
       assign(:forum_validation, @fv)
       render
       expect(rendered).to include I18n.t('forum_validations.show.not_validated')
+    end
+
+    it 'should NOT display links to edit each the mods' do
+      @fv.validated = false
+      assign(:forum_validation, @fv)
+      render
+      expect(rendered).to_not have_link('', href: edit_mod_path(id: 'potato'))
+      expect(rendered).to_not have_link('', href: edit_mod_path(id: 'salad'))
+      expect(rendered).to_not have_link('', href: edit_mod_path(id: 'simulator'))
     end
   end
 
